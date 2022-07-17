97a101c861a6b459eae0dc0a1f5adfe28d298611
AnKhly <91781956+AnKhly@users.noreply.github.com>
2022-07-17 14:03:00 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(num) {
+	let arr = [];
+	arr.push(num);
+	let a = arr.join().split('').reverse();
+	if (a.indexOf('-') !== -1) {
+		a.splice(a.indexOf('-'), 1)
+	}
+	let b = a.map(function (v) { return v = v * 1 }).join('') * 1;
+	return b;
 }
